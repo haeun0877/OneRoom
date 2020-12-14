9e15e62d3bdb38b75dff72b1da5cfc584ab1101c
haeun0877 <70643208+haeun0877@users.noreply.github.com>
2020-12-14 23:59:00 +0900

    Update Interaction

---
+++ Disappear.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+    [SerializeField] float disappearTime;
+
+    private void OnEnable() //Disappear가 붙어있는 객체가 활성화 돼있을 때 마다 실행되는 것
+    {
+        StartCoroutine(DisapperCoroutine());
+    }
+
+    IEnumerator DisapperCoroutine()
+    {
+        yield return new WaitForSeconds(disappearTime);
+
+        gameObject.SetActive(false);
+    }
+}
---
+++ InteractionController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+
+    RaycastHit hitInfo;
+
+    [SerializeField] GameObject go_NomalCrosshair;
+    [SerializeField] GameObject go_InteractiveCrosshair;
+
+    bool isContact = false;
+    bool isInteract = false;
+
+    [SerializeField] ParticleSystem ps_QuestionEffect;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckObject();
+    }
+
+    void CheckObject()
+    {
+        Vector3 t_MousPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+        if (Physics.Raycast(cam.ScreenPointToRay(t_MousPos), out hitInfo, 100))
+        {
+            Contact();
+            ClickLeftButton();
+        }
+        else
+        {
+            NotContact();
+        }
+    }
+
+    void Contact()
+    {
+        if (hitInfo.transform.CompareTag("Interaction"))
+        {
+            if (!isContact)
+            {
+                isContact = true;
+                go_InteractiveCrosshair.SetActive(true);
+                go_NomalCrosshair.SetActive(false);
+            }
+        }
+        else
+        {
+            NotContact();
+        }
+    }
+
+    void NotContact()
+    {
+        if (isContact)
+        {
+            isContact = false;
+            go_InteractiveCrosshair.SetActive(false);
+            go_NomalCrosshair.SetActive(true);
+        }
+    }
+
+    void ClickLeftButton()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isContact)
+            {
+                Interact();
+            }
+        }
+    }
+
+    void Interact()
+    {
+        isInteract = true;
+
+        ps_QuestionEffect.gameObject.SetActive(true);
+        Vector3 t_targetPos = hitInfo.transform.position;
+        ps_QuestionEffect.GetComponent<QuestionEffect>().SetTarget(t_targetPos);
+        ps_QuestionEffect.transform.position = cam.transform.position;
+    }
+}
